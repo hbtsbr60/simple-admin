379bdb650a8aff3d4b3da7dac32523687810544e
Usman Suleiman Babakolo <usmansbk@gmail.com>
2022-02-04 15:06:00 +0100

    abstract theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,20 @@
-import React, { useMemo, useState, createContext } from "react";
-import {
-  Button,
-  CssBaseline,
-  PaletteMode,
-  createTheme,
-  responsiveFontSizes,
-} from "@mui/material";
+import React from "react";
+import { Button, CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@emotion/react";
-import { grey } from "@mui/material/colors";
-
-const ColorModeContext = createContext({ toggleColorMode: () => {} });
-
-const getDesignTokens = (mode: PaletteMode) => ({
-  palette: {
-    mode,
-    ...(mode === "light"
-      ? {
-          primary: {
-            main: "#3f20ba",
-          },
-          text: {
-            primary: grey[900],
-            secondary: grey[800],
-          },
-        }
-      : {
-          primary: {
-            main: "#3f20ba",
-          },
-          text: {
-            primary: "#fff",
-            secondary: grey[500],
-          },
-        }),
-  },
-});
+import { ColorModeProvider, useAppTheme } from "config/theme";
 
 function App() {
-  const [mode, setMode] = useState<PaletteMode>("dark");
-  const colorMode = useMemo(
-    () => ({
-      toggleColorMode: () => {
-        setMode((prevMode: PaletteMode) =>
-          prevMode === "light" ? "dark" : "light"
-        );
-      },
-    }),
-    []
-  );
-
-  const theme = useMemo(
-    () => responsiveFontSizes(createTheme(getDesignTokens(mode))),
-    [mode]
-  );
+  const { colorMode, theme } = useAppTheme();
 
   return (
-    <ColorModeContext.Provider value={colorMode}>
+    <ColorModeProvider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Button variant="contained" onClick={colorMode.toggleColorMode}>
           Hello World
         </Button>
       </ThemeProvider>
-    </ColorModeContext.Provider>
+    </ColorModeProvider>
   );
 }
 
---
+++ src/config/theme.ts
@@ -0,0 +1,55 @@
+import { useMemo, useState, createContext, useContext } from "react";
+import { PaletteMode, createTheme, responsiveFontSizes } from "@mui/material";
+import { grey } from "@mui/material/colors";
+
+const ColorModeContext = createContext({ toggleColorMode: () => {} });
+
+export const ColorModeProvider = ColorModeContext.Provider;
+
+const getDesignTokens = (mode: PaletteMode) => ({
+  palette: {
+    mode,
+    ...(mode === "light"
+      ? {
+          primary: {
+            main: "#3f20ba",
+          },
+          text: {
+            primary: grey[900],
+            secondary: grey[800],
+          },
+        }
+      : {
+          primary: {
+            main: "#3f20ba",
+          },
+          text: {
+            primary: "#fff",
+            secondary: grey[500],
+          },
+        }),
+  },
+});
+
+export const useColorMode = () => useContext(ColorModeContext);
+
+export const useAppTheme = () => {
+  const [mode, setMode] = useState<PaletteMode>("dark");
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode: PaletteMode) =>
+          prevMode === "light" ? "dark" : "light"
+        );
+      },
+    }),
+    []
+  );
+
+  const theme = useMemo(
+    () => responsiveFontSizes(createTheme(getDesignTokens(mode))),
+    [mode]
+  );
+
+  return { colorMode, theme };
+};
