35708831575fb7f4a852e75bf48c095e03abcc82
Usman Suleiman Babakolo <usmansbk@gmail.com>
2022-02-19 22:28:08 +0100

    add form elements

--- src/pages/users/NewUserForm.tsx
+++ src/pages/users/NewUserForm.tsx
@@ -1,5 +1,62 @@
+import { Box, Button, Stack, TextField } from "@mui/material";
+import { useIntl } from "react-intl";
+
 function NewUserForm() {
-  return <h1>Add User</h1>;
+  const t = useIntl();
+
+  return (
+    <Box maxWidth={400}>
+      <Stack component="form" spacing={1}>
+        <Stack direction={{ xs: "column", sm: "row" }} spacing={1}>
+          <TextField
+            fullWidth
+            id="firstName"
+            variant="filled"
+            label={t.formatMessage({ id: "label.firstName" })}
+          />
+          <TextField
+            fullWidth
+            id="lastName"
+            variant="filled"
+            label={t.formatMessage({ id: "label.lastName" })}
+            InputLabelProps={{ shrink: true }}
+          />
+        </Stack>
+        <TextField
+          id="username"
+          variant="filled"
+          label={t.formatMessage({ id: "label.username" })}
+          InputLabelProps={{ shrink: true }}
+        />
+        <TextField
+          type="email"
+          id="email"
+          variant="filled"
+          label={t.formatMessage({ id: "label.email" })}
+          InputLabelProps={{ shrink: true }}
+        />
+        <TextField
+          type="tel"
+          id="phoneNumber"
+          variant="filled"
+          label={t.formatMessage({ id: "label.phoneNumber" })}
+          InputLabelProps={{ shrink: true }}
+        />
+        <TextField
+          type="password"
+          id="password"
+          variant="filled"
+          label={t.formatMessage({ id: "label.password" })}
+          InputLabelProps={{ shrink: true }}
+        />
+        <Box>
+          <Button variant="contained">
+            {t.formatMessage({ id: "button.addUser" })}
+          </Button>
+        </Box>
+      </Stack>
+    </Box>
+  );
 }
 
 export default NewUserForm;
