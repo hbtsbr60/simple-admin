bd2bc98c53ddb59b0316822387b3568a1f01027c
Usman Suleiman Babakolo <usmansbk@gmail.com>
2022-02-26 07:19:19 +0100

    add validation

--- src/pages/permissions/NewPermissionForm.tsx
+++ src/pages/permissions/NewPermissionForm.tsx
@@ -1,29 +1,97 @@
-import { Box, Button, Stack, TextField } from "@mui/material";
+import { useMemo, useState } from "react";
+import { useForm } from "react-hook-form";
 import { useIntl } from "react-intl";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Box, Button, Stack, TextField } from "@mui/material";
+
+type FormData = {
+  name: string;
+  action: string;
+  resource: string;
+  description: string;
+};
 
 function NewPermissionForm() {
   const t = useIntl();
+  const schema = useMemo(
+    () =>
+      yup
+        .object({
+          name: yup
+            .string()
+            .required(t.formatMessage({ id: "error.name.required" }))
+            .max(64, t.formatMessage({ id: "error.text.tooLong" }))
+            .test(
+              "isAlphaNumeric",
+              t.formatMessage(
+                { id: "error.text.alphaNumeric" },
+                { field: "Name" }
+              ),
+              (value = "") => /^[a-zA-Z0-9_]+$/.test(value)
+            ),
+          action: yup
+            .string()
+            .required(t.formatMessage({ id: "error.action.required" }))
+            .max(64, t.formatMessage({ id: "error.text.tooLong" })),
+          resource: yup
+            .string()
+            .required(t.formatMessage({ id: "error.resource.required" }))
+            .max(64, t.formatMessage({ id: "error.text.tooLong" })),
+          description: yup
+            .string()
+            .max(240, t.formatMessage({ id: "error.text.tooLong" })),
+        })
+        .required(),
+    [t.locale]
+  );
+
+  const [permissions, setPermissions] = useState<Array<FormData>>([]);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, touchedFields },
+    reset,
+  } = useForm<FormData>({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = handleSubmit((values) => {
+    setPermissions([...permissions, values]);
+    reset();
+  });
 
   return (
-    <Box component="form" autoComplete="off" maxWidth={800}>
+    <Box component="form" autoComplete="off" maxWidth={800} onSubmit={onSubmit}>
       <Stack spacing={1}>
         <TextField
           id="name"
           variant="filled"
           label={t.formatMessage({ id: "label.name" })}
           InputLabelProps={{ shrink: true }}
+          {...register("name")}
+          error={touchedFields.name && !!errors.name}
+          helperText={touchedFields?.name && errors.name?.message}
         />
         <TextField
           id="action"
           variant="filled"
           label={t.formatMessage({ id: "label.action" })}
           InputLabelProps={{ shrink: true }}
+          placeholder={t.formatMessage({ id: "placeholder.action" })}
+          {...register("action")}
+          error={touchedFields.action && !!errors.action}
+          helperText={touchedFields?.action && errors.action?.message}
         />
         <TextField
           id="resource"
           variant="filled"
           label={t.formatMessage({ id: "label.resource" })}
           InputLabelProps={{ shrink: true }}
+          placeholder={t.formatMessage({ id: "placeholder.resource" })}
+          {...register("resource")}
+          error={touchedFields.resource && !!errors.resource}
+          helperText={touchedFields?.resource && errors.resource?.message}
         />
         <TextField
           multiline
@@ -32,9 +100,12 @@ function NewPermissionForm() {
           variant="filled"
           label={t.formatMessage({ id: "label.description" })}
           InputLabelProps={{ shrink: true }}
+          {...register("description")}
+          error={touchedFields.description && !!errors.description}
+          helperText={touchedFields?.description && errors.description?.message}
         />
         <Box>
-          <Button variant="contained">
+          <Button type="submit" variant="contained">
             {t.formatMessage({ id: "button.addPermission" })}
           </Button>
         </Box>
